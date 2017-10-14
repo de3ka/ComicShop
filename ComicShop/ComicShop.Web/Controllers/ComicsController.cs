6907254a0f48d3853bec5b27201535b2b3b51701
de3ka <derketo@abv.bg>
2017-10-15 02:08:12 +0300

    comics controller added

---
+++ ComicShop/ComicShop.Web/Controllers/ComicsController.cs
@@ -0,0 +1,44 @@
+﻿using ComicShop.Data.Services.Contracts;
+using ComicShop.Web.Models;
+using Bytes2you.Validation;
+using System.Linq;
+using System.Web.Mvc;
+using PagedList;
+
+namespace ComicShop.Web.Controllers
+{
+    public class ComicsController : Controller
+    {
+        private readonly IComicService comicService;
+
+        public ComicsController(IComicService comicService)
+        {
+            Guard.WhenArgument(comicService, "comicService").IsNull().Throw();
+
+            this.comicService = comicService;
+        }
+
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            var allComics = this.comicService.GetAllComics().ToList().Select(c => new ComicViewModel(c)).ToList();
+            return View(allComics.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                var targetComic = this.comicService.GetComicById((int)id);
+
+                return View(targetComic);
+            }
+            else
+            {
+                return View(this.comicService.GetAllComics().First());
+            }
+        }
+    }
+}
