7d83f8cd19d215db05327afba286f45bb496362a
de3ka <derketo@abv.bg>
2017-10-15 01:56:25 +0300

    ninject dependency resolver

---
+++ ComicShop/ComicShop.Web/IoC/NinjectDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ComicShop.Web.IoC
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        public void Dispose() { }
+    }
+}
