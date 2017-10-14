ffbc114294abe692715c8933f0bdfd3e266749b8
de3ka <derketo@abv.bg>
2017-10-15 02:07:38 +0300

    db config file added

---
+++ ComicShop/ComicShop.Web/App_Start/DbConfig.cs
@@ -0,0 +1,15 @@
+﻿using ComicShop.Data;
+using ComicShop.Data.Migrations;
+using System.Data.Entity;
+
+namespace ComicShop.Web.App_Start
+{
+    public class DbConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ComicShopDbContext, Configuration>());
+            ComicShopDbContext.Create().Database.CreateIfNotExists();
+        }
+    }
+}
