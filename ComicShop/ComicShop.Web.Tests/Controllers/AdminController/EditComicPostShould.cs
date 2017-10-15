a74f79ac3cf232344c8db48a0cfe79d541105058
de3ka <derketo@abv.bg>
2017-10-15 13:37:56 +0300

    admin controller edit comic post tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/AdminController/EditComicPostShould.cs
@@ -0,0 +1,50 @@
+﻿using ComicShop.Data.Models;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class EditComicPostShould
+    {
+        [Test]
+        public void RedirectToIndexActionResultWhenModelStateIsValid()
+        {
+            //Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+            Comic comic = new Comic();
+
+            //Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                  comicServiceMock.Object, orderServiceMock.Object);
+
+            //Assert
+            adminController
+                .WithCallTo(c => c.EditComic(comic))
+                .ShouldRedirectTo(x => x.Index);
+        }
+
+        [Test]
+        public void ReturnDefaultViewWhenModelStateIsNotValid()
+        {
+            //Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+            Comic comic = new Comic();
+
+            //Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                  comicServiceMock.Object, orderServiceMock.Object);
+
+            adminController.ModelState.AddModelError("testError", "error detected");
+
+            //Assert
+            adminController
+                .WithCallTo(c => c.EditComic(comic))
+                .ShouldRenderDefaultView();
+        }
+    }
+}
