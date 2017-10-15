da211975c6b9967c5a89d73c6906a22ce49e8ec1
de3ka <derketo@abv.bg>
2017-10-15 03:15:37 +0300

    refactoring

--- ComicShop/ComicShop.Web/Global.asax.cs
+++ ComicShop/ComicShop.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicShop.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,10 +13,13 @@ namespace ComicShop.Web
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbConfig.Initialize();
         }
     }
 }
