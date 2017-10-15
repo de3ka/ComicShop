8d77870f975304a76ee8a27fb3278caba0aca275
de3ka <derketo@abv.bg>
2017-10-15 04:02:43 +0300

    repository detach tests

---
+++ ComicShop/ComicShop.Web.Tests/Data/EfComicShopDataProvider/DetachShould.cs
@@ -0,0 +1,35 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models.Contracts;
+using ComicShop.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Data.Entity;
+
+namespace ComicShop.Web.Tests.Data.EfComicShopDataProvider
+{
+    [TestFixture]
+    public class DetachShould
+    {
+        [Test]
+        public void BeCalledWhenInvoked()
+        {
+            // Arrange
+            var mockedSet = new Mock<DbSet<IComic>>();
+            var mockedDbContext = new Mock<IComicShopDbContext>();
+            mockedDbContext.Setup(x => x.Set<IComic>()).Returns(mockedSet.Object);
+            var dataProvider = new EfComicShopDataProvider<IComic>(mockedDbContext.Object);
+            var mockedAdvert = new Mock<IComic>();
+
+            // Act
+            try
+            {
+                dataProvider.Detach(mockedAdvert.Object);
+            }
+            catch (NullReferenceException) { };
+
+            // Assert
+            mockedDbContext.Verify(x => x.Entry(mockedAdvert.Object), Times.AtLeastOnce);
+        }
+    }
+}
