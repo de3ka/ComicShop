88789a0404df540b8d3208a2607291d8a07d315e
de3ka <derketo@abv.bg>
2017-10-15 13:11:43 +0300

    comics controller constructor tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/ComicsController/ConstructorShould.cs
@@ -0,0 +1,31 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ComicShop.Web.Tests.Controllers.ComicsController
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ReturnsAnInstanceWhenParameterIsNotNull()
+        {
+            // Arrange
+            var comicServiceMock = new Mock<IComicService>();
+
+            // Act
+            var comicsController = new ComicShop.Web.Controllers.ComicsController(comicServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(comicsController);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenParametersAreNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ComicShop.Web.Controllers.ComicsController(null));
+        }
+    }
+}
