71025ad9594de62099ff9dbfc9c5ad091462790c
de3ka <derketo@abv.bg>
2017-10-15 03:44:41 +0300

    comic service get by id tests

---
+++ ComicShop/ComicShop.Web.Tests/Services/ComicService/GetAllComicsShould.cs
@@ -0,0 +1,46 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace ComicShop.Web.Tests.Services.ComicService
+{
+    [TestFixture]
+    public class GetAllComicsShould
+    {
+        [Test]
+        public void CallComicDataProviderAllMethod()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+
+            //Act
+            var actualComicService =
+                new ComicShop.Data.Services.ComicService(mockedDataProvider.Object);
+
+            actualComicService.GetAllComics();
+
+            //Assert
+            mockedDataProvider.Verify(
+                service => service.All(),
+                Times.Once);
+        }
+
+        [Test]
+        public void ReturnIqueryableCollectionFromCOMICDataProviderAllMethod()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+
+            //Act
+            var actualComicService =
+                new ComicShop.Data.Services.ComicService(mockedDataProvider.Object);
+
+            var actualResult = actualComicService.GetAllComics();
+
+            //Assert
+            Assert.That(actualResult, Is.Not.Null.And.InstanceOf<IQueryable<Comic>>());
+        }
+    }
+}
