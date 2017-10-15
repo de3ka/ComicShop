83033a72d428204a3e9e8bf2c9ab3472d9e2be91
de3ka <derketo@abv.bg>
2017-10-15 13:02:06 +0300

    home controller constructor tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/HomeController/ConstructorShould.cs
@@ -0,0 +1,31 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ComicShop.Web.Tests.Controllers.HomeController
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ReturnsAnInstanceWhenParameterIsNotNull()
+        {
+            // Arrange
+            var comicServiceMock = new Mock<IComicService>();
+
+            // Act
+            var homeController = new ComicShop.Web.Controllers.HomeController(comicServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(homeController);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenParametersAreNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ComicShop.Web.Controllers.HomeController(null));
+        }
+    }
+}
