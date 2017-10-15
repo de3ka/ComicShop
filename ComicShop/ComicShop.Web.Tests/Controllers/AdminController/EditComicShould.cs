50912c978903d1352342745775d4d78a5fa8c10b
de3ka <derketo@abv.bg>
2017-10-15 13:37:28 +0300

    admin controller edit comic tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/AdminController/EditComicShould.cs
@@ -0,0 +1,37 @@
+﻿using ComicShop.Data.Models;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class EditComicShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            //Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+            Comic comic = new Comic() { Id = 1 };
+
+            comicServiceMock.Setup(x => x.GetComicById(1)).Returns(comic);
+
+            //Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                  comicServiceMock.Object, orderServiceMock.Object);
+
+            //Assert
+            adminController
+                .WithCallTo(c => c.EditComic(1))
+                .ShouldRenderDefaultView();
+        }
+    }
+}
