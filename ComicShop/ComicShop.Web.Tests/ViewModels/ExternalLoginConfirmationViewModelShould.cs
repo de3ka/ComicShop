fa6c4908daa98b8eac351f4b689596fe4690c222
de3ka <derketo@abv.bg>
2017-10-15 04:08:56 +0300

    external login tests

---
+++ ComicShop/ComicShop.Web.Tests/ViewModels/ExternalLoginConfirmationViewModelShould.cs
@@ -0,0 +1,22 @@
+﻿using ComicShop.Web.Models;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.ViewModels
+{
+    [TestFixture]
+    public class ExternalLoginConfirmationViewModelShould
+    {
+        [Test]
+        public void SetCorrectlyAllPropertiesWhenInitialized()
+        {
+            //Arrange
+            var externalLoginConfirmationViewModel = new ExternalLoginConfirmationViewModel();
+            string email = "john@gmail.com";
+            //Act
+            externalLoginConfirmationViewModel.Email = email;
+
+            //Assert
+            Assert.AreEqual(externalLoginConfirmationViewModel.Email, email);
+        }
+    }
+}
