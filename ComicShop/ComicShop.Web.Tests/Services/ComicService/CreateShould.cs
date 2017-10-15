40a6361ea525e4f64a3196845f4f36f5c391121c
de3ka <derketo@abv.bg>
2017-10-15 03:44:13 +0300

    comic service create tests

---
+++ ComicShop/ComicShop.Web.Tests/Services/ComicService/CreateShould.cs
@@ -0,0 +1,67 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ComicShop.Web.Tests.Services.ComicService
+{
+    [TestFixture]
+    public class CreateShould
+    {
+        [Test]
+        public void CallCreatureDataProviderAddMethodWithSameRecievedComic()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            var mockedComic = new Mock<Comic>();
+
+            //Act
+            var actualComicService =
+                new ComicShop.Data.Services.ComicService(mockedDataProvider.Object);
+
+            actualComicService.Create(mockedComic.Object);
+
+            //Assert
+            mockedDataProvider.Verify(
+                service => service.Add(mockedComic.Object),
+                Times.Once);
+        }
+
+        [Test]
+        public void CallComicDataProviderSaveChangesMethod()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            var mockedComic = new Mock<Comic>();
+
+            //Act
+            var actualComicService =
+                new ComicShop.Data.Services.ComicService(mockedDataProvider.Object);
+
+            actualComicService.Create(mockedComic.Object);
+
+            //Assert
+            mockedDataProvider.Verify(
+                service => service.SaveChanges(),
+                Times.Once);
+        }
+
+        [Test]
+        public void ThrowWhenArgumentComicHasNullValue()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            Comic nullComic = null;
+
+            //Act
+            var actualComicService =
+                new ComicShop.Data.Services.ComicService(mockedDataProvider.Object);
+
+            //Assert
+            Assert.That(
+                () => actualComicService.Create(nullComic),
+                Throws.InstanceOf<ArgumentNullException>());
+        }
+    }
+}
