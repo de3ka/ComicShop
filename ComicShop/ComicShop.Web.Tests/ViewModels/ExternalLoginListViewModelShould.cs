2be1696d2f923e855f623fbec042a81996c53e0f
de3ka <derketo@abv.bg>
2017-10-15 04:09:13 +0300

    external login tests

---
+++ ComicShop/ComicShop.Web.Tests/ViewModels/ExternalLoginListViewModelShould.cs
@@ -0,0 +1,22 @@
+﻿using ComicShop.Web.Models;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.ViewModels
+{
+    [TestFixture]
+    public class ExternalLoginListViewModelShould
+    {
+        [Test]
+        public void SetCorrectlyAllPropertiesWhenInitialized()
+        {
+            //Arrange
+            var externalLoginListViewModel = new ExternalLoginListViewModel();
+            string url = "www.google.com";
+            //Act
+            externalLoginListViewModel.ReturnUrl = url;
+
+            //Assert
+            Assert.AreEqual(externalLoginListViewModel.ReturnUrl, url);
+        }
+    }
+}
