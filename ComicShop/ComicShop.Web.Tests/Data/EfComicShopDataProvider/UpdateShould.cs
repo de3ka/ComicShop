2c08e885423721456a991b4aa24ba233f00d6710
de3ka <derketo@abv.bg>
2017-10-15 04:03:13 +0300

    repository update tests

---
+++ ComicShop/ComicShop.Web.Tests/Data/EfComicShopDataProvider/UpdateShould.cs
@@ -0,0 +1,35 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models.Contracts;
+using ComicShop.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicShop.Web.Tests.Data.EfComicShopDataProvider
+{
+    [TestFixture]
+    public class UpdateShould
+    {
+        [Test]
+        public void ThrowNullReferenceExceptionWhenPassedArgumentIsNull()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<IComicShopDbContext>();
+            var mockedSet = new Mock<DbSet<IComic>>();
+
+            // Act
+            mockedDbContext.Setup(set => set.Set<IComic>()).Returns(mockedSet.Object);
+            var dataProvider = new EfComicShopDataProvider<IComic>(mockedDbContext.Object);
+            IComic entity = null;
+
+            // Assert
+            Assert.That(() => dataProvider.Update(entity),
+                Throws.InstanceOf<NullReferenceException>());
+        }
+    }
+}
