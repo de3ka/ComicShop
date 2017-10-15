2381850bde0c8d4e263dd1875735724b26c0003d
de3ka <derketo@abv.bg>
2017-10-15 12:54:07 +0300

    shopping cart controller out of stock tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/ShoppingCartController/OutOfStockShould.cs
@@ -0,0 +1,27 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.ShoppingCartController
+{
+    [TestFixture]
+    public class OutOfStockShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+
+            // Arrange
+            var comicServiceMock = new Mock<IComicService>();
+
+            // Act
+            var shoppingCartController = new ComicShop.Web.Controllers.ShoppingCartController(comicServiceMock.Object);
+
+            //Assert
+            shoppingCartController
+                .WithCallTo(c => c.OutOfStock())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
