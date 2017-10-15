5c0bf194d02170a865d9c14a58f5cd74ea71fb21
de3ka <derketo@abv.bg>
2017-10-15 03:54:07 +0300

    data configuration tests

---
+++ ComicShop/ComicShop.Web.Tests/Data/Configuration/ConstructorShould.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Data.Configuration
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void SetPropertiesAutomaticMigrationsEnabledAndAutomaticMigrationDataLossAllowedToTrue()
+        {
+            var config = new ComicShop.Data.Migrations.Configuration();
+
+            Assert.AreEqual(config.AutomaticMigrationsEnabled, true);
+            Assert.AreEqual(config.AutomaticMigrationDataLossAllowed, true);
+        }
+    }
+}
