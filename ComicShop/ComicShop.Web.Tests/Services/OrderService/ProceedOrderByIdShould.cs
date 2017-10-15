f588e102b136108f7f5240e443e568c0425dc152
de3ka <derketo@abv.bg>
2017-10-15 03:47:18 +0300

    order service proceed order by id tests

---
+++ ComicShop/ComicShop.Web.Tests/Services/OrderService/ProceedOrderByIdShould.cs
@@ -0,0 +1,83 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Services.OrderService
+{
+    [TestFixture]
+    public class ProceedOrderByIdShould
+    {
+        [Test]
+        public void CallOrderDataProviderGetByIdMethod()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfComicShopDataProvider<Order>>();
+            var mockedComicDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            var orderId = 5;
+            var mockedOrderToUpdate = new Order() { Id = orderId };
+
+
+            mockedOrderDataProvider.Setup(x => x.GetById(orderId)).Returns(mockedOrderToUpdate);
+
+            //Act
+            var actualOrderService = new ComicShop.Data.Services.OrderService(
+                mockedOrderDataProvider.Object,
+                mockedComicDataProvider.Object,
+                mockedOrderToUpdate);
+
+            actualOrderService.ProceedOrderById(orderId);
+
+            //Assert
+            mockedOrderDataProvider.Verify(x => x.GetById(orderId), Times.Once);
+        }
+
+        [Test]
+        public void CallOrderDataProviderSaveChangesMethod()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfComicShopDataProvider<Order>>();
+            var mockedComicDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            var orderId = 5;
+            var mockedOrderToUpdate = new Order() { Id = orderId };
+
+
+            mockedOrderDataProvider.Setup(x => x.GetById(orderId)).Returns(mockedOrderToUpdate);
+
+            //Act
+            var actualOrderService = new ComicShop.Data.Services.OrderService(
+                mockedOrderDataProvider.Object,
+                mockedComicDataProvider.Object,
+                mockedOrderToUpdate);
+
+            actualOrderService.ProceedOrderById(orderId);
+
+            //Assert
+            mockedOrderDataProvider.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void SetOrderToUpdateIsProceededValueToTrue()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfComicShopDataProvider<Order>>();
+            var mockedComicDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            var orderId = 5;
+            var mockedOrderToUpdate = new Order() { Id = orderId, isProceeded = false };
+
+
+            mockedOrderDataProvider.Setup(x => x.GetById(orderId)).Returns(mockedOrderToUpdate);
+
+            //Act
+            var actualOrderService = new ComicShop.Data.Services.OrderService(
+                mockedOrderDataProvider.Object,
+                mockedComicDataProvider.Object,
+                mockedOrderToUpdate);
+
+            actualOrderService.ProceedOrderById(orderId);
+
+            //Assert
+            Assert.AreEqual(mockedOrderToUpdate.isProceeded, true);
+        }
+    }
+}
