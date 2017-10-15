b6102a71d2eff47ca4dbe9d2dcd6eeae0a3b9e79
de3ka <derketo@abv.bg>
2017-10-15 04:09:35 +0300

    login view model tests

---
+++ ComicShop/ComicShop.Web.Tests/ViewModels/LoginViewModelShould.cs
@@ -0,0 +1,28 @@
+﻿using ComicShop.Web.Models;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.ViewModels
+{
+    [TestFixture]
+    public class LoginViewModelShould
+    {
+        [Test]
+        public void SetCorrectlyAllPropertiesWhenInitialized()
+        {
+            //Arrange
+            var loginViewModel = new LoginViewModel();
+            string username = "John";
+            string password = "Agent007";
+
+            //Act
+            loginViewModel.UserName = username;
+            loginViewModel.Password = password;
+            loginViewModel.RememberMe = false;
+
+            //Assert
+            Assert.AreEqual(loginViewModel.UserName, username);
+            Assert.AreEqual(loginViewModel.Password, password);
+            Assert.AreEqual(loginViewModel.RememberMe, false);
+        }
+    }
+}
