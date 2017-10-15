9db6b15738cae5b5f6a3d3a0b16d264e4ef012b5
de3ka <derketo@abv.bg>
2017-10-15 04:02:15 +0300

    repository add tests

---
+++ ComicShop/ComicShop.Web.Tests/Data/EfComicShopDataProvider/AddShould.cs
@@ -0,0 +1,33 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models.Contracts;
+using ComicShop.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Runtime.Serialization;
+
+namespace ComicShop.Web.Tests.Data.EfComicShopDataProvider
+{
+    [TestFixture]
+    public class AddShould
+    {
+        [Test]
+        public void ThrowNullReferenceExceptionWhenPassedArgumentIsNull()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<IComicShopDbContext>();
+            var mockedSet = new Mock<DbSet<IComic>>();
+
+            // Act
+            mockedDbContext.Setup(set => set.Set<IComic>()).Returns(mockedSet.Object);
+            var dataProvider = new EfComicShopDataProvider<IComic>(mockedDbContext.Object);
+            IComic entity = null;
+
+            // Assert
+            Assert.That(() => dataProvider.Add(entity),
+                Throws.InstanceOf<NullReferenceException>());
+        }
+    }
+}
