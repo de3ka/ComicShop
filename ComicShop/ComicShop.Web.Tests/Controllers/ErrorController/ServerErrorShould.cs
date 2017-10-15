89969a19631ae943a33264d8d036d8624d238058
de3ka <derketo@abv.bg>
2017-10-15 13:08:08 +0300

    error controller server error tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/ErrorController/ServerErrorShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.ErrorController
+{
+    [TestFixture]
+    public class ServerErrorShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            //Arrange & Act
+            var errorController = new ComicShop.Web.Controllers.ErrorController();
+
+            //Assert
+            errorController
+                .WithCallTo(c => c.ServerError())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
