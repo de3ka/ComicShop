ec023fe4eeb08550dd9f4b24118876f9f2c484d1
de3ka <derketo@abv.bg>
2017-10-15 03:43:51 +0300

    comic service constructor test

---
+++ ComicShop/ComicShop.Web.Tests/Services/ComicService/ConstructorShould.cs
@@ -0,0 +1,37 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ComicShop.Web.Tests.Services.ComicService
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void InitializeNewInstanceOfIComicServiceWhenProperParameterIsPassed()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+
+            //Act & Assert
+            Assert.That(
+                () => new ComicShop.Data.Services.ComicService(mockedDataProvider.Object),
+                Is.InstanceOf<IComicService>());
+        }
+
+        [Test]
+        public void ThrowWhenNullParameterIsPassed()
+        {
+            //Arrange
+            IEfComicShopDataProvider<Comic> nullDataProvider = null;
+
+            //Act & Assert
+            Assert.That(
+                () => new ComicShop.Data.Services.ComicService(nullDataProvider),
+                Throws.InstanceOf<ArgumentNullException>());
+        }
+    }
+}
