955c5f7d0e4c397f7bbc643df5875e581afa16d4
de3ka <derketo@abv.bg>
2017-10-15 03:46:00 +0300

    order service constructor tests

---
+++ ComicShop/ComicShop.Web.Tests/Services/OrderService/ConstructorShould.cs
@@ -0,0 +1,29 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Services.OrderService
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void InitializeNewInstanceOfIOrderServiceWhenProperParameterIsPassed()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfComicShopDataProvider<Order>>();
+            var mockedComicDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+            var mockedOrderToCreate = new Mock<Order>();
+
+            //Act & Assert
+            Assert.That(
+                () => new ComicShop.Data.Services.OrderService(
+                    mockedOrderDataProvider.Object,
+                    mockedComicDataProvider.Object,
+                    mockedOrderToCreate.Object),
+                Is.InstanceOf<IOrderService>());
+        }
+    }
+}
