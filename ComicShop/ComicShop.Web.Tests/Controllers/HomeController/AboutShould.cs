57a0d4ed3fd422eedbdc87287cebd7ba935f663f
de3ka <derketo@abv.bg>
2017-10-15 13:02:58 +0300

    home controller about tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/HomeController/AboutShould.cs
@@ -0,0 +1,30 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.HomeController
+{
+    [TestFixture]
+    public class AboutShould
+    {
+        [Test]
+        public void ReturnViewWithListOfCorrectModelsWhenThereAreAnyExisting()
+        {
+            // Arrange
+            var comicServiceMock = new Mock<IComicService>();
+
+            // Act
+            var homeController = new ComicShop.Web.Controllers.HomeController(comicServiceMock.Object);
+            var viewResult = homeController.About() as ViewResult;
+
+            //Assert
+            homeController
+                .WithCallTo(c => c.About())
+                .ShouldRenderDefaultView();
+
+            Assert.AreEqual("Your application description page.", viewResult.ViewData["Message"]);
+        }
+    }
+}
