fdaa142e4750dcb05071ab0ed366131eddce15a7
de3ka <derketo@abv.bg>
2017-10-15 03:33:50 +0300

    user model tests

---
+++ ComicShop/ComicShop.Web.Tests/Models/UserModelShould.cs
@@ -0,0 +1,31 @@
+﻿using ComicShop.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Models
+{
+    [TestFixture]
+    public class UserModelShould
+    {
+        [Test]
+        public void SetCorrectlyAllPropertiesWhenInitialized()
+        {
+            //Arrange
+            User actualUser = new User()
+            {
+                UserName = "John",
+                Email = "john@gmail.com",
+                Address = "London"
+            };
+
+            //Assert
+            Assert.That(
+                () => new User(),
+                Is.InstanceOf<IdentityUser>());
+
+            Assert.AreEqual(actualUser.UserName, "John");
+            Assert.AreEqual(actualUser.Email, "john@gmail.com");
+            Assert.AreEqual(actualUser.Address, "London");
+        }
+    }
+}
