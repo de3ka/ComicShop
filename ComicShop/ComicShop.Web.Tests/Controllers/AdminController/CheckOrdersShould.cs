dd93d42c9baf172b817c62c6e3cee1a7de644104
de3ka <derketo@abv.bg>
2017-10-15 13:41:05 +0300

    admin controller check orders tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/AdminController/CheckOrdersShould.cs
@@ -0,0 +1,34 @@
+﻿using ComicShop.Data.Models;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class CheckOrdersShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            //Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+            Order order = new Order();
+            var ordersList = new List<Order>() { order };
+            orderServiceMock.Setup(x => x.GetAllOrders()).Returns(ordersList.AsQueryable);
+
+            //Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                  comicServiceMock.Object, orderServiceMock.Object);
+
+            //Assert
+            adminController
+                .WithCallTo(c => c.CheckOrders())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
