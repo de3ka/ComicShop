454ad32e5323570a4bcfbe63420280f386277736
de3ka <derketo@abv.bg>
2017-10-15 03:45:15 +0300

    comic service get by id tests

---
+++ ComicShop/ComicShop.Web.Tests/Services/ComicService/GetComicByIdShould.cs
@@ -0,0 +1,30 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Services.ComicService
+{
+    [TestFixture]
+    public class GetComicByIdShould
+    {
+        [TestCase(1)]
+        [TestCase(5)]
+        public void CallComicDataProviderGetAllMethodWithSameId(int correctId)
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfComicShopDataProvider<Comic>>();
+
+            //Act
+            var actualComicService =
+                new ComicShop.Data.Services.ComicService(mockedDataProvider.Object);
+
+            actualComicService.GetComicById(correctId);
+
+            //Assert
+            mockedDataProvider.Verify(
+                service => service.GetById(correctId),
+                Times.Once);
+        }
+    }
+}
