336646b3e5f26e75d2ecf24d3e24e033613e7b5f
de3ka <derketo@abv.bg>
2017-10-15 13:36:33 +0300

    admin controller add comic tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/AdminController/AddComicShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class AddComicShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            //Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+
+            //Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                  comicServiceMock.Object, orderServiceMock.Object);
+
+            //Assert
+            adminController
+                .WithCallTo(c => c.AddComic())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
