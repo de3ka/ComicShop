f7216c437f9edb97e03e8110c818a525f3492e77
de3ka <derketo@abv.bg>
2017-10-15 03:51:35 +0300

    helpers for testing data layer

---
+++ ComicShop/ComicShop.Web.Tests/Helpers/DbContextForTest.cs
@@ -0,0 +1,13 @@
+﻿using ComicShop.Data;
+using System.Data.Entity;
+
+namespace ComicShop.Web.Tests.Helpers
+{
+    public class DbContextForTest : ComicShopDbContext
+    {
+        public void TestModelCreation(DbModelBuilder model)
+        {
+            OnModelCreating(model);
+        }
+    }
+}
---
+++ ComicShop/ComicShop.Web.Tests/Helpers/DbSetForTest.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+
+namespace ComicShop.Web.Tests.Helpers
+{
+    public class DbSetForTest<T> : DbSet<T> where T : class
+    {
+    }
+}
