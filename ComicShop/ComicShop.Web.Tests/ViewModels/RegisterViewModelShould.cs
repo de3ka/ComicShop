32f5d951148e81be7fe2aa12ee4914274db2f6af
de3ka <derketo@abv.bg>
2017-10-15 04:09:54 +0300

    register view model tests

---
+++ ComicShop/ComicShop.Web.Tests/ViewModels/RegisterViewModelShould.cs
@@ -0,0 +1,39 @@
+﻿using ComicShop.Web.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicShop.Web.Tests.ViewModels
+{
+    [TestFixture]
+    public class RegisterViewModelShould
+    {
+        [Test]
+        public void SetCorrectlyAllPropertiesWhenInitialized()
+        {
+            //Arrange
+            var registerViewModel = new RegisterViewModel();
+            string username = "John";
+            string password = "Agent007";
+            string email = "john@gmail.com";
+            string address = "Bulgaria, Sofia";
+
+            //Act
+            registerViewModel.UserName = username;
+            registerViewModel.Email = email;
+            registerViewModel.Address = address;
+            registerViewModel.Password = password;
+            registerViewModel.ConfirmPassword = password;
+
+            //Assert
+            Assert.AreEqual(registerViewModel.UserName, username);
+            Assert.AreEqual(registerViewModel.Email, email);
+            Assert.AreEqual(registerViewModel.Address, address);
+            Assert.AreEqual(registerViewModel.ConfirmPassword, password);
+            Assert.AreEqual(registerViewModel.Password, password);
+        }
+    }
+}
