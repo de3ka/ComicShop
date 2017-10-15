830879aefba13ad390d73619da36cb504b1ffcae
de3ka <derketo@abv.bg>
2017-10-15 13:30:55 +0300

    admin controller constructor tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/AdminController/ConstructorShould.cs
@@ -0,0 +1,56 @@
+﻿using ComicShop.Data.Services;
+using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ComicShop.Web.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ReturnsAnInstanceWhenParameterIsNotNull()
+        {
+            // Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+
+            // Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                comicServiceMock.Object, orderServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(adminController);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenParametersAreNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ComicShop.Web.Areas.Admin.Controllers.AdminController(null, null));
+        }
+
+        [Test]
+        public void ThrowExceptionWhenComicServiceIsNull()
+        {
+            // Arrange
+            ComicService comicServiceMock = null;
+            var orderServiceMock = new Mock<IOrderService>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ComicShop.Web.Areas.Admin.Controllers.AdminController(comicServiceMock, orderServiceMock.Object));
+        }
+
+        [Test]
+        public void ThrowExceptionWhenOrderServiceIsNull()
+        {
+            // Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            OrderService orderServiceMock = null;
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ComicShop.Web.Areas.Admin.Controllers.AdminController(comicServiceMock.Object, orderServiceMock));
+        }
+    }
+}
