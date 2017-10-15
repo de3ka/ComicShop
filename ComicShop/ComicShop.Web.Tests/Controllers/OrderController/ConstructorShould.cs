6a31f731b2add4d5a19b1f61ad556008b238cdd9
de3ka <derketo@abv.bg>
2017-10-15 13:00:01 +0300

    order controller constructor tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/OrderController/ConstructorShould.cs
@@ -0,0 +1,31 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ComicShop.Web.Tests.Controllers.OrderController
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ReturnsAnInstanceWhenParameterIsNotNull()
+        {
+            // Arrange
+            var orderServiceMock = new Mock<IOrderService>();
+
+            // Act
+            var orderController = new ComicShop.Web.Controllers.OrderController(orderServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(orderController);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenParametersAreNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ComicShop.Web.Controllers.OrderController(null));
+        }
+    }
+}
