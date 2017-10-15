dc38f3b5f02126158e143a6f3ab96bae0bfc746a
de3ka <derketo@abv.bg>
2017-10-15 03:52:40 +0300

    dbcontext tested

---
+++ ComicShop/ComicShop.Web.Tests/Data/ComicShopDbContext/ConstructorShould.cs
@@ -0,0 +1,48 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models;
+using ComicShop.Web.Tests.Helpers;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Data.ComicShopDbContext
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void HaveParameterlessConstructor()
+        {
+            // Arrange & Act
+            var context = new ComicShop.Data.ComicShopDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<ComicShop.Data.ComicShopDbContext>(context);
+        }
+
+        [Test]
+        public void Return_InstanceOfIComicShopDbContext()
+        {
+            // Arrange & Act
+            var context = new ComicShop.Data.ComicShopDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<IComicShopDbContext>(context);
+        }
+
+        [Test]
+        public void SetComicsDbSetAndOrdersDbSet()
+        {
+            // Arrange
+            var context = new ComicShop.Data.ComicShopDbContext();
+            var comics = new DbSetForTest<Comic>();
+            var orders = new DbSetForTest<Order>();
+
+            //Act
+            context.Comics = comics;
+            context.Orders = orders;
+
+            //Assert
+            Assert.AreEqual(context.Comics, comics);
+            Assert.AreEqual(context.Orders, orders);
+        }
+    }
+}
---
+++ ComicShop/ComicShop.Web.Tests/Data/ComicShopDbContext/CreateShould.cs
@@ -0,0 +1,20 @@
+﻿using ComicShop.Data.Contracts;
+using NUnit.Framework;
+
+namespace ComicShop.Web.Tests.Data.ComicShopDbContext
+{
+    [TestFixture]
+    public class CreateShould
+    {
+        [Test]
+        public void ReturnNewDbContextInstance()
+        {
+            // Arrange & Act
+            var newContext = ComicShop.Data.ComicShopDbContext.Create();
+
+            // Assert
+            Assert.IsNotNull(newContext);
+            Assert.IsInstanceOf<IComicShopDbContext>(newContext);
+        }
+    }
+}
