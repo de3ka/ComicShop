72fe76cd33c5cc48fb587cc5ea40637974750f79
de3ka <derketo@abv.bg>
2017-10-15 13:01:15 +0300

    order controller index tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/OrderController/IndexShould.cs
@@ -0,0 +1,26 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.OrderController
+{
+    [TestFixture]
+    public class IndexShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            // Arrange
+            var orderServiceMock = new Mock<IOrderService>();
+
+            // Act
+            var comicController = new ComicShop.Web.Controllers.OrderController(orderServiceMock.Object);
+
+            //Assert
+            comicController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
