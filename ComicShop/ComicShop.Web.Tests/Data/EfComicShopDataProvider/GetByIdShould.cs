ef427519a6fba3f8c22baf4120c76ffb8ce1d69c
de3ka <derketo@abv.bg>
2017-10-15 04:03:43 +0300

    repository get by id tests

---
+++ ComicShop/ComicShop.Web.Tests/Data/EfComicShopDataProvider/GetByIdShould.cs
@@ -0,0 +1,32 @@
+﻿using ComicShop.Data.Contracts;
+using ComicShop.Data.Models.Contracts;
+using ComicShop.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System.Data.Entity;
+
+namespace ComicShop.Web.Tests.Data.EfComicShopDataProvider
+{
+    [TestFixture]
+    public class GetByIdShould
+    {
+        [Test]
+        public void ReturnCorrectResultWhenParameterIsValid()
+        {
+            // Arrange 
+            var mockedDbSet = new Mock<DbSet<IComic>>();
+            var mockedDbContext = new Mock<IComicShopDbContext>();
+            var fakeAdvert = new Mock<IComic>();
+            var validId = 15;
+
+            // Act
+            mockedDbContext.Setup(mock => mock.Set<IComic>()).Returns(mockedDbSet.Object);
+            var dataProvider = new EfComicShopDataProvider<IComic>(mockedDbContext.Object);
+            mockedDbSet.Setup(x => x.Find(It.IsAny<int>())).Returns(fakeAdvert.Object);
+
+            // Assert
+            Assert.That(dataProvider.GetById(validId), Is.Not.Null);
+            Assert.AreEqual(dataProvider.GetById(validId), fakeAdvert.Object);
+        }
+    }
+}
