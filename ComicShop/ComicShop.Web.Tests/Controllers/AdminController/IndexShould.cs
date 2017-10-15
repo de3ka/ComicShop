11e97ace8b556c516aecfe7b6e9c4e4e7f868273
de3ka <derketo@abv.bg>
2017-10-15 13:40:32 +0300

    admin controller index tests

---
+++ ComicShop/ComicShop.Web.Tests/Controllers/AdminController/IndexShould.cs
@@ -0,0 +1,28 @@
+﻿using ComicShop.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace ComicShop.Web.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class IndexShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            //Arrange
+            var comicServiceMock = new Mock<IComicService>();
+            var orderServiceMock = new Mock<IOrderService>();
+
+            //Act
+            var adminController = new ComicShop.Web.Areas.Admin.Controllers.AdminController(
+                  comicServiceMock.Object, orderServiceMock.Object);
+
+            //Assert
+            adminController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
