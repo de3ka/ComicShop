414b10956fefb2b3825da3afcf251749ed9ddb0b
de3ka <derketo@abv.bg>
2017-10-15 03:11:24 +0300

    ninject

--- ComicShop/ComicShop.Web/App_Start/NinjectWeb.cs
+++ ComicShop/ComicShop.Web/App_Start/NinjectWeb.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ComicShop.Web.App_Start.NinjectWeb), "Start")]
 
 namespace ComicShop.Web.App_Start
 {
-    public class NinjectWeb
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject.Web;
+
+    public static class NinjectWeb
     {
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+        }
     }
 }
